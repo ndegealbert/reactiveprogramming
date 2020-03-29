050f843c222faa374cd91517df4b7d95f459d1a4
Albert <35229852+ndegealbert@users.noreply.github.com>
2020-03-29 01:10:01 -0400

    Observable
    Observable.create() and new Observable() essentially do the same thing.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import "./styles.css";
-import {fromEvent} from 'rxjs'
-import  { Observable }  from  'rxjs'
+import { fromEvent } from "rxjs";
+import { Observable } from "rxjs";
 
-var  button =  document.getElementById('fetch')
-fromEvent('click',button)
+var button = document.getElementById("fetch");
+fromEvent("click", button);
+const source$ = Observable.create(function(subscribe) {
+  subscribe.next(1);
+  subscribe.next(2);
+  subscribe.next(3);
 
-const source$ =Observable.create(function(observer){
-    observer.next(1)
-    observer.next(2)
-    observer.next(3)
-    observer.complete("completeted")
+});
+
+source$.subscribe({
+    next(val) {console.log(val)},
+    error(e) {console.log(e)},
+    complete() {console.log("completed")}
 })
 
-source$.subscribe(
- console.log
-)
+const source1$ = new Observable(observer => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.complete("the execution completed")
+});
+
+source1$.subscribe({
+    next(val) {console.log(val)},
+    error(e) {console.log(e)},
+    complete() {console.log("completed")}
+})
